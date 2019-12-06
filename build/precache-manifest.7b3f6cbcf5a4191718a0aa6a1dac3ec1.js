78ea05455021f7ab02fcfea208120cbe78131a7d
Vinayak Mandrekar <k.vinayak05@gmail.com>
2019-12-06 23:08:12 +0530

    deploying React To-Do-App

---
+++ build/precache-manifest.7b3f6cbcf5a4191718a0aa6a1dac3ec1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01821936fd884bee2e2ba927c62c7ce1",
+    "url": "/react-to-do-app/index.html"
+  },
+  {
+    "revision": "89501bd1bf9bb4102de3",
+    "url": "/react-to-do-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d2f69eec76627fe94208",
+    "url": "/react-to-do-app/static/js/2.59bce4c2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-to-do-app/static/js/2.59bce4c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89501bd1bf9bb4102de3",
+    "url": "/react-to-do-app/static/js/main.3db456b4.chunk.js"
+  },
+  {
+    "revision": "3c1a3c1d45c1133ae880",
+    "url": "/react-to-do-app/static/js/runtime-main.ad27700a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-to-do-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+
 
 function App() {
+  let [items, setItems] = useState();
+
+  let [list, setList] = useState([]);
+
+  function handleInput(event) {
+    //console.log(event.target.value);
+    let item = event.target.value;
+    setItems(item);
+  }
+
+  function addList(event) {
+    //list = items;
+    console.log(list);
+    setList(function(prev) {
+      return [...prev, items];
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleInput} type="text" />
+        <button onClick={addList}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {list.map(function(items) {
+            return <li>{items}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
